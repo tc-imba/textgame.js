a1cf4101a252f1d0a7fca0948046ac4e0c5abd5d
tc-imba <liuyh615@sjtu.edu.cn>
2017-07-30 00:50:36 +0800

    Initial commit

---
+++ src/event.js
@@ -0,0 +1,12 @@
+class Event {
+
+    _id;
+
+    constructor(id) {
+        this._id = id;
+
+    }
+
+}
+
+module.exports = Event;
---
+++ src/eventengine.js
@@ -0,0 +1,19 @@
+const Event = require('./event');
+
+class EventEngine {
+
+    _events = [];
+    _eventCount = 0;
+
+    createEvent(options = {}) {
+        const id = this._eventCount++;
+        const event = new Event(id);
+        this._events.push(event);
+
+    }
+
+
+}
+
+const instance = new EventEngine();
+module.exports = instance;
